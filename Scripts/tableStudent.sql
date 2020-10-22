31661c51b7c73fc7743f8996aaa8c0bd3e37c985
kralos-7 <c_rojasx@hotmail.com>
2020-10-22 17:20:29 -0500

    scripts y gif de configuración de la conexión a la BD

---
+++ Scripts/tableStudent.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS student;
+
+CREATE TABLE IF NOT EXISTS student (
+    _id INT AUTO_INCREMENT PRIMARY KEY,
+    _name VARCHAR(30) NOT NULL,
+    _ntable VARCHAR(20) NOT NULL
+);
+
+INSERT INTO student (_name,_ntable) 
+VALUES  ("Carlos","Pagos"),
+        ("Cesar","Usuarios"),
+        ("Everardo","Alumno"),
+        ("Jonathan","Beca"),
+        ("Kevin","Tutorias"),
+        ("Libia","Calificaciones"),
+        ("Zurisadai","Profesor"),
+        ("Yara","Asignatura");
